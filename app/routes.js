0e6fe4485be222b581d65173f0ea5f936ed9c0b6
andrii mutylo <amutylo@gmail.com>
2016-11-09 13:01:52 +0200

    update events controller

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,14 @@
 const express = require('express'),
   router = express.Router(),
   mainController = require('./controllers/main.controller');
+  eventsController = require('./controllers/events.controller');
 
 // export router
 module.exports = router;
 
 //create routes
-
+//main routes
 router.get('/', mainController.showHome);
+
+//event routes
+router.get('/events', eventControllers.showEvents);
